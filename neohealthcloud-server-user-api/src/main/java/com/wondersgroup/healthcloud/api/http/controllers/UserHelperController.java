213e4231cf64af339916e76d576bd23247b7ecb4
zhangzhixiu <zhixiu1018@gmail.com>
2016-08-01 10:34:15 +0800

    update spring boot to 1.4.0

--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/UserHelperController.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/UserHelperController.java
@@ -5,9 +5,9 @@ import com.wondersgroup.healthcloud.common.http.support.misc.JsonKeyReader;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
 import com.wondersgroup.healthcloud.services.user.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +22,7 @@ public class UserHelperController {
      *
      * @return
      */
-    @RequestMapping(value = "/user/feedback", method = RequestMethod.POST)
+    @PostMapping(path = "/user/feedback")
     @VersionRange
     public JsonResponseEntity<String> saveFeedback(@RequestBody String request) {
         JsonResponseEntity<String> response = new JsonResponseEntity<>();
