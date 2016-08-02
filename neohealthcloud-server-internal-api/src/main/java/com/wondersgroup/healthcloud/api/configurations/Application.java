a2adbd321fb6c908e966c049501ce38637c7d052
zhangzhixiu <zhixiu1018@gmail.com>
2016-08-02 11:33:53 +0800

    internal api add configuration.

--- neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/configurations/Application.java
+++ neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/configurations/Application.java
@@ -1,10 +1,16 @@
 package com.wondersgroup.healthcloud.api.configurations;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.wondersgroup.common.jail.property.JailStartListener;
+import java.util.Properties;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
@@ -23,14 +29,24 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication(scanBasePackages = "com.wondersgroup.healthcloud")
 public class Application extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-    }
+  public static void main(String[] args) throws Exception {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.addListeners(buildListener());
+    app.run(args);
+  }
+
+  private static JailStartListener buildListener() throws Exception {
+    ResourceLoader resourceLoader = new DefaultResourceLoader();
+    Resource resource = resourceLoader.getResource("config/application-de.properties");
+    Properties properties = new Properties();
+    properties.load(resource.getInputStream());
+    return new JailStartListener(new OkHttpClient(), properties.getProperty("jail.host"),
+        properties.getProperty("jail.group"));
+  }
 }
