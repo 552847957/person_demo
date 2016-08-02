0ae98fa972a6bbfe1c410f3489cdf1bb65bb2888
zhangzhixiu <zhixiu1018@gmail.com>
2016-08-02 17:44:34 +0800

    add doctor pool

---
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/utils/easemob/EasemobDoctorPool.java
@@ -0,0 +1,37 @@
+package com.wondersgroup.healthcloud.utils.easemob;
+
+import com.wondersgroup.healthcloud.common.utils.IdGen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
+ * ░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
+ * ░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌
+ * ░█▌▓▓▓▀▀▓▓▓▓███▓▓▓▓▓▓▓▄▀▓▓▐█
+ * ▐█▐██▐░▄▓▓▓▓▓▀▄░▀▓▓▓▓▓▓▓▓▓▌█▌
+ * █▌███▓▓▓▓▓▓▓▓▐░░▄▓▓███▓▓▓▄▀▐█
+ * █▐█▓▀░░▀▓▓▓▓▓▓▓▓▓██████▓▓▓▓▐█
+ * ▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌
+ * ▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌
+ * █▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐█
+ *
+ * Created by zhangzhixiu on 8/2/16.
+ */
+@Component
+public class EasemobDoctorPool {
+
+  @Autowired
+  private EasemobAccountUtil util;
+
+  public EasemobAccount fetchOne() {
+    String id = "d" + IdGen.uuid();//easemob doctor account id has 33 length and start with character 'd'
+    String pwd = IdGen.uuid();
+    Boolean result = util.register(id, pwd, id);
+    if (result) {
+      return new EasemobAccount(id, pwd);
+    } else {
+      return null;
+    }
+  }
+}
--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/UserHelperController.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/UserHelperController.java
@@ -10,49 +10,52 @@ import com.wondersgroup.healthcloud.common.http.support.misc.JsonKeyReader;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
 import com.wondersgroup.healthcloud.services.user.FeedbackService;
 import com.wondersgroup.healthcloud.utils.easemob.EasemobAccountUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api")
 public class UserHelperController {
 
-    @Autowired
-    private FeedbackService feedbackService;
+  @Autowired
+  private FeedbackService feedbackService;
 
-    @Autowired
-    private EasemobAccountUtil util;
+  @Autowired
+  private EasemobAccountUtil util;
 
-    @Autowired
-    private HttpRequestExecutorManager httpRequestExecutorManager;
+  @Autowired
+  private HttpRequestExecutorManager httpRequestExecutorManager;
 
-    /**
-     * 意见反馈
-     *
-     * @return
-     */
-    @PostMapping(path = "/user/feedback")
-    @VersionRange
-    public JsonResponseEntity<String> saveFeedback(@RequestBody String request) {
-        JsonResponseEntity<String> response = new JsonResponseEntity<>();
-        JsonKeyReader reader = new JsonKeyReader(request);
-        String uid = reader.readString("uid", true);
-        String comments = reader.readString("comments", false);
-        String contact = reader.readString("contact", true);
+  /**
+   * 意见反馈
+   */
+  @PostMapping(path = "/user/feedback")
+  @VersionRange
+  public JsonResponseEntity<String> saveFeedback(@RequestBody String request) {
+    JsonResponseEntity<String> response = new JsonResponseEntity<>();
+    JsonKeyReader reader = new JsonKeyReader(request);
+    String uid = reader.readString("uid", true);
+    String comments = reader.readString("comments", false);
+    String contact = reader.readString("contact", true);
 
-        feedbackService.saveFeedback(uid, comments, contact, "0");
-        response.setData("反馈成功");
-        return response;
-    }
+    feedbackService.saveFeedback(uid, comments, contact, "0");
+    response.setData("反馈成功");
+    return response;
+  }
 
-    @GetMapping(path = "/user/test")
-    @VersionRange
-    public void test(HttpServletResponse response) throws Exception {
-        Request request = new RequestBuilder().get().url("http://www.baidu.com").build();
-        StringResponseWrapper wrapper = (StringResponseWrapper) httpRequestExecutorManager.newCall(request).run().as(StringResponseWrapper.class);
-        ByteStreams.copy(wrapper.nativeResponse().body().byteStream(), response.getOutputStream());
-    }
-
+  @GetMapping(path = "/user/test")
+  @VersionRange
+  public void test(HttpServletResponse response) throws Exception {
+    Request request = new RequestBuilder().get().url("http://www.baidu.com").build();
+    StringResponseWrapper wrapper =
+        (StringResponseWrapper) httpRequestExecutorManager.newCall(request)
+            .run()
+            .as(StringResponseWrapper.class);
+    ByteStreams.copy(wrapper.nativeResponse().body().byteStream(), response.getOutputStream());
+  }
 }
