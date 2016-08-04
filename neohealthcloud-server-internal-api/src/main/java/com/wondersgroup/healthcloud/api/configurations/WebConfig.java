5d1a80a1cb23d36ffcbefbb05c03987cf8dca422
李玄武 <410230697@qq.com>
2016-08-04 17:40:04 +0800

    更新cors support

--- neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/configurations/WebConfig.java
+++ neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/configurations/WebConfig.java
@@ -16,9 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import javax.servlet.DispatcherType;
 import java.util.List;
@@ -86,24 +84,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return registration;
     }
 
-    @Bean
-    public FilterRegistrationBean corsFilterRegistration() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE");
     }
-
 }
