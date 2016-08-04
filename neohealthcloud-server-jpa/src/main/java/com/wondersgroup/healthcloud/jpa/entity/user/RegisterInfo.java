86a0a28e1a5117c4641287450611500b5b423ba8
longshasha <longshasha@wondersgroup.com>
2016-08-04 13:50:28 +0800

    base

---
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/entity/user/RegisterInfo.java
@@ -0,0 +1,69 @@
+package com.wondersgroup.healthcloud.jpa.entity.user;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by longshasha on 16/8/4.
+ */
+@Data
+@Entity
+@Table(name = "app_tb_register_info")
+public class RegisterInfo {
+    @Id
+    private String registerid; //主键
+
+    private String userid; //用户id
+
+    private String usertype; //用户类型（1.患者，2.医生）
+
+    private String identifytype; //认证类型（0.未认证，1.认证）
+
+    private String personcard; //身份证
+
+    private String regmobilephone; //手机号
+
+    @Column(name = "bind_personcard")
+    private String bindPersoncard; //医养结合服务绑定的身份证
+
+    private String medicarecard;//医保卡
+
+    private String regPassword; //密码
+
+    private String headphoto; //头像
+
+    private Date regtime; //注册时间
+
+    private String nickname; //昵称
+
+    private String talkid; //环信id
+    private String talkpwd; //环信pwd
+    private String name;
+    private String gender;
+    private Date birthday;
+    private String tagid;
+    private Integer channelType; // 渠道类型：1:健康云、2:QQ、3:微信、4:微博
+
+    @Column(name = "del_flag")
+    private String delFlag;
+
+    @Column(name = "source_id")
+    private String sourceId;
+
+    @Column(name = "create_by")
+    private String createBy;
+
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @Column(name = "update_by")
+    private String updateBy;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+}
---
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/user/RegisterInfoRepository.java
@@ -0,0 +1,25 @@
+package com.wondersgroup.healthcloud.jpa.repository.user;
+
+import com.wondersgroup.healthcloud.jpa.entity.user.RegisterInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * Created by longshasha on 16/8/4.
+ */
+public interface RegisterInfoRepository extends JpaRepository<RegisterInfo,String> {
+
+    @Query("select r from RegisterInfo r where r.talkid =?1 and r.delFlag='0'")
+    RegisterInfo findByTalkid(String talkid);
+
+    @Query("select r from RegisterInfo r where r.regmobilephone = ?1 and r.delFlag='0'")
+    RegisterInfo findByMobile(String mobile);
+
+    @Query("select r from RegisterInfo r where r.personcard =?1 and r.identifytype!='0' and r.delFlag='0'")
+    List<RegisterInfo> findByPersoncard(String personcard);
+
+    @Query("select r from RegisterInfo r where r.registerid =?1 and r.delFlag='0'")
+    RegisterInfo findByRegisterid(String registerId);
+}
