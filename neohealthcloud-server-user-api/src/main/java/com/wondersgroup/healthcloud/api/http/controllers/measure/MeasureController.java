b79271f271d06c0010eb6132f0ef5eac929077c3
李玄武 <410230697@qq.com>
2016-08-21 20:39:13 +0800

    update mesure app api

--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
@@ -1,5 +1,7 @@
 package com.wondersgroup.healthcloud.api.http.controllers.measure;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.wondersgroup.healthcloud.api.http.dto.measure.SimpleMeasure;
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
@@ -8,7 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.*;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -30,6 +36,12 @@ public class MeasureController {
     private static final String requestFamilyPath = "%s/api/measure/family/nearest?%s";
 
     private static final String requestUploadPath = "%s/api/measure/upload/%s";
+
+    private static final String requestChartPath = "%s/api/measure/chart/%s?%s";
+
+    private static final String requestDayHistoryPath = "%s/api/measure/dayHistory/?%s";
+
+    private static final String requestYearHistoryPath = "%s/api/measure/yearHistory/%s?%s";
 
     private RestTemplate template = new RestTemplate();
 
@@ -80,7 +92,6 @@ public class MeasureController {
     @VersionRange
     @PostMapping("upload/{type}")
     public JsonResponseEntity<?> uploadMeasureIndexs(@PathVariable int type, @RequestBody Map<String, Object> paras) {
-
         try {
             String url = String.format(requestUploadPath, env.getProperty("measure.server.host"), type);
             HttpHeaders headers = new HttpHeaders();
@@ -88,12 +99,94 @@ public class MeasureController {
             headers.add("access-token", "version3.0");
             ResponseEntity<Map> response = template.postForEntity(url, new HttpEntity<>(paras, headers), Map.class);
             if (response.getStatusCode().equals(HttpStatus.OK)) {
-                return new JsonResponseEntity<>(0, "数据上传成功", response.getBody().get("data"));
+                if (0 == (int) response.getBody().get("code")) {
+                    return new JsonResponseEntity<>(0, "数据上传成功", response.getBody().get("data"));
+                }
             }
         } catch (RestClientException e) {
             log.info("上传体征数据失败", e);
         }
         return new JsonResponseEntity<>(1000, "数据上传失败");
     }
+
+    /**
+     * 查询测量趋势图
+     *
+     * @param type       测量类型
+     * @param registerId 注册认证码
+     * @param flag       分页 页码
+     * @return
+     */
+    @VersionRange
+    @GetMapping("chart/{type}")
+    public JsonResponseEntity queryMeasureChart(@PathVariable int type, String registerId, int flag) {
+        try {
+            String params = "registerId=".concat(registerId).concat("&flag=").concat(String.valueOf(flag));
+            String url = String.format(requestChartPath, env.getProperty("measure.server.host"), type, params);
+            ResponseEntity<Map> response = template.getForEntity(url, Map.class);
+            if (response.getStatusCode().equals(HttpStatus.OK)) {
+                if (0 == (int) response.getBody().get("code")) {
+                    return new JsonResponseEntity<>(0, "数据查询成功", response.getBody().get("data"));
+                }
+            }
+        } catch (Exception e) {
+            log.info("type =" + type + "的图表数据获取失败", e);
+        }
+        return new JsonResponseEntity(1000, "数据获取失败");
+    }
+
+    /**
+     * 查询历史记录
+     * 根据年的维度展示,不分页
+     *
+     * @param type       测量类型
+     * @param registerId 注册认证码
+     * @return
+     * @throws JsonProcessingException
+     */
+    @VersionRange
+    @GetMapping("yearHistory/{type}")
+    public JsonResponseEntity queryMeasureHistory(@PathVariable int type, String registerId) throws JsonProcessingException {
+        try {
+            String url = String.format(requestYearHistoryPath, env.getProperty("measure.server.host"), type, "registerId=".concat(registerId));
+            ResponseEntity<Map> response = template.getForEntity(url, Map.class);
+            if (response.getStatusCode().equals(HttpStatus.OK)) {
+                if (0 == (int) response.getBody().get("code")) {
+                    return new JsonResponseEntity<>(0, "历史数据查询成功", response.getBody().get("data"));
+                }
+            }
+        } catch (Exception e) {
+            log.info("type =" + type + "的历史数据查询失败", e);
+        }
+        return new JsonResponseEntity(1000, "历史数据查询失败");
+    }
+
+    /**
+     * 查询历史记录
+     * 根据天的维度展示
+     *
+     * @param registerId 注册认证码
+     * @param flag       分页 测量时间
+     * @return
+     * @throws JsonProcessingException
+     */
+    @GetMapping("dayHistory")
+    public JsonResponseEntity queryMeasureHistory(String registerId, String flag) throws JsonProcessingException {
+        try {
+            String param = "registerId=".concat(registerId);
+            String params = (flag == null) ? param : param.concat("&flag=").concat(flag);
+            String url = String.format(requestDayHistoryPath, env.getProperty("measure.server.host"), params);
+            ResponseEntity<Map> response = template.getForEntity(url, Map.class);
+            if (response.getStatusCode().equals(HttpStatus.OK)) {
+                if (0 == (int) response.getBody().get("code")) {
+                    return new JsonResponseEntity<>(0, "近期数据查询成功", response.getBody().get("data"));
+                }
+            }
+        } catch (Exception e) {
+            log.info("近期历史数据获取失败", e);
+        }
+        return new JsonResponseEntity(1000, "近期历史数据获取失败");
+    }
+
 
 }
