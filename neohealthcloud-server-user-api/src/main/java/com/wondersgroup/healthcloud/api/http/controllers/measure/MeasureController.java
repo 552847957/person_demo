c9e55dd48a1dd1ff112273fbc452d02c4ed3cb2a
zhangzhixiu <zhixiu1018@gmail.com>
2016-08-27 19:54:52 +0800

    measure controller host update.

--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
@@ -1,8 +1,6 @@
 package com.wondersgroup.healthcloud.api.http.controllers.measure;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import com.wondersgroup.healthcloud.api.http.dto.measure.SimpleMeasure;
 import com.wondersgroup.healthcloud.api.utils.JacksonHelper;
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
@@ -11,17 +9,17 @@ import com.wondersgroup.healthcloud.services.measure.MeasureManagementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.*;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Jeffrey on 16/8/19.
@@ -31,6 +29,9 @@ import java.util.*;
 public class MeasureController {
 
     private static final Logger log = LoggerFactory.getLogger(MeasureController.class);
+
+    @Value("${internal.api.service.measure.url}")
+    private String host;
 
     private static final String requestFamilyPath = "%s/api/measure/family/nearest?%s";
 
@@ -57,8 +58,8 @@ public class MeasureController {
 
         List histories = new ArrayList();
         String parameters = "registerId=".concat(registerId);
-        String URL = String.format(requestDayHistoriesListPath, env.getProperty("measure.server.host"), parameters);
-        ResponseEntity<Map> response = template.getForEntity(URL, Map.class);
+        String url = String.format(requestDayHistoriesListPath, host, parameters);
+        ResponseEntity<Map> response = template.getForEntity(url, Map.class);
         if (response.getStatusCode().equals(HttpStatus.OK)) {
             Map body = response.getBody();
             if (0 == (int) body.get("code")) {
@@ -73,7 +74,7 @@ public class MeasureController {
         JsonResponseEntity<Map> result = new JsonResponseEntity<>(0, null);
         result.setData(homeMap);
         Map<Class, String[]> filters = new HashMap<>();
-        filters.put(MeasureManagement.class, new String[]{"id","createdDate","lastModifiedDate", "createdBy", "lastModifiedBy", "display"});
+        filters.put(MeasureManagement.class, new String[]{"id", "createdDate", "lastModifiedDate", "createdBy", "lastModifiedBy", "display"});
         return JacksonHelper.getInstance().serializeExclude(filters).writeValueAsString(result);
     }
 
@@ -82,7 +83,7 @@ public class MeasureController {
     public JsonResponseEntity<?> nearestMeasure(@RequestParam String familyMateId) {
         try {
             String parameters = "registerId=".concat(familyMateId).concat("&personCard=0");
-            String url = String.format(requestFamilyPath, env.getProperty("measure.server.host"), parameters);
+            String url = String.format(requestFamilyPath, host, parameters);
             ResponseEntity<Map> response = template.getForEntity(url, Map.class);
             if (response.getStatusCode().equals(HttpStatus.OK)) {
                 Map<String, Object> responseBody = response.getBody();
@@ -99,7 +100,7 @@ public class MeasureController {
     @PostMapping("upload/{type}")
     public JsonResponseEntity<?> uploadMeasureIndexs(@PathVariable int type, @RequestBody Map<String, Object> paras) {
         try {
-            String url = String.format(requestUploadPath, env.getProperty("measure.server.host"), type);
+            String url = String.format(requestUploadPath, host, type);
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
             headers.add("access-token", "version3.0");
@@ -128,7 +129,7 @@ public class MeasureController {
     public JsonResponseEntity queryMeasureChart(@PathVariable int type, String registerId, int flag) {
         try {
             String params = "registerId=".concat(registerId).concat("&flag=").concat(String.valueOf(flag));
-            String url = String.format(requestChartPath, env.getProperty("measure.server.host"), type, params);
+            String url = String.format(requestChartPath, host, type, params);
             ResponseEntity<Map> response = template.getForEntity(url, Map.class);
             if (response.getStatusCode().equals(HttpStatus.OK)) {
                 if (0 == (int) response.getBody().get("code")) {
@@ -154,7 +155,7 @@ public class MeasureController {
     @GetMapping("yearHistory/{type}")
     public JsonResponseEntity queryMeasureHistory(@PathVariable int type, String registerId) throws JsonProcessingException {
         try {
-            String url = String.format(requestYearHistoryPath, env.getProperty("measure.server.host"), type, "registerId=".concat(registerId));
+            String url = String.format(requestYearHistoryPath, host, type, "registerId=".concat(registerId));
             ResponseEntity<Map> response = template.getForEntity(url, Map.class);
             if (response.getStatusCode().equals(HttpStatus.OK)) {
                 if (0 == (int) response.getBody().get("code")) {
@@ -181,7 +182,7 @@ public class MeasureController {
         try {
             String param = "registerId=".concat(registerId);
             String params = (flag == null) ? param : param.concat("&flag=").concat(flag);
-            String url = String.format(requestDayHistoryPath, env.getProperty("measure.server.host"), params);
+            String url = String.format(requestDayHistoryPath, host, params);
             ResponseEntity<Map> response = template.getForEntity(url, Map.class);
             if (response.getStatusCode().equals(HttpStatus.OK)) {
                 if (0 == (int) response.getBody().get("code")) {
