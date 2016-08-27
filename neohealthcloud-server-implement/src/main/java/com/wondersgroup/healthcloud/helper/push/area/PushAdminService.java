20711cdfb936ce373911ec5e4e219dccdafa8188
zhangzhixiu <zhixiu1018@gmail.com>
2016-08-27 14:24:09 +0800

    push admin service.

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/push/area/PushAdminService.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/push/area/PushAdminService.java
@@ -1,6 +1,8 @@
 package com.wondersgroup.healthcloud.helper.push.area;
 
 import com.wondersgroup.healthcloud.helper.push.getui.PushAdminClient;
+import com.wondersgroup.healthcloud.jpa.entity.app.UserPushInfo;
+import com.wondersgroup.healthcloud.jpa.repository.app.UserPushInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,14 +26,28 @@ import java.util.List;
 public class PushAdminService {
 
     private PushAdminSelector selector;
+    private UserPushInfoRepository userPushInfoRepository;
 
     public void setTags(String cid, String area, List<String> tags) {
         PushAdminClient adminClient = selector.getByArea(area, false);
         adminClient.overrideTagToClient(cid, tags);
     }
 
+    public void setTags(String uid, List<String> tags) {
+        UserPushInfo userPushInfo = userPushInfoRepository.findByUid(uid);
+        if (userPushInfo != null) {
+            PushAdminClient adminClient = selector.getByArea(userPushInfo.getArea(), false);
+            adminClient.overrideTagToClient(userPushInfo.getCid(), tags);
+        }
+    }
+
     @Autowired
     public void setSelector(PushAdminSelector selector) {
         this.selector = selector;
+    }
+
+    @Autowired
+    public void setUserPushInfoRepository(UserPushInfoRepository userPushInfoRepository) {
+        this.userPushInfoRepository = userPushInfoRepository;
     }
 }
