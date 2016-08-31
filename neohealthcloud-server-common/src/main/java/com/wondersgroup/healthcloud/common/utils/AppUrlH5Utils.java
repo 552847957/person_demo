4afca9782f2d0baf1edde5b93534dc97c5d89637
yanshuai <yanshuai@wondersgroup.com>
2016-08-31 15:25:40 +0800

    健康档案 h5 url build

--- neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/utils/AppUrlH5Utils.java
+++ neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/utils/AppUrlH5Utils.java
@@ -25,7 +25,6 @@ public class AppUrlH5Utils {
     }
 
     public String buildDiseaseArticleView(int articleId) {
-
         return "";
     }
 
@@ -40,11 +39,40 @@ public class AppUrlH5Utils {
 
     /**
      * 学苑 文章 详情
-     * @param articleId
-     * @return
      */
     public String buildXueYuanArticleView(int articleId) {
         //for_type 用于判断 app进入h5页面的时候，是否需要去请求 检查改h5用户时候收藏过,以及分享信息等
-        return basePath + "web/article/xueyuan/articleView?from=doctor&for_type=article&id="+articleId;
+        return basePath + "/web/article/xueyuan/articleView?from=doctor&for_type=article&id="+articleId;
     }
+
+    /**
+     * 健康档案 - 住院记录
+     */
+    public String buildHealthRecordZhuyuan(String idc){
+        return basePath + "/healthRecords/operationList?idc="+idc;
+    }
+
+    /**
+     * 健康档案 - 就诊报告
+     */
+    public String buildHealthRecordJiuzhen(String idc){
+        return basePath + "/healthRecords/seedoctorList?idc="+idc;
+    }
+
+
+    /**
+     * 健康档案 - 检验报告
+     */
+    public String buildHealthRecordJianyan(String idc){
+        return basePath + "/healthRecords/examList?idc="+idc;
+    }
+
+
+    /**
+     * 健康档案 - 用药列表
+     */
+    public String buildHealthRecordYongyao(String idc){
+        return basePath + "/healthRecords/drugList?idc="+idc;
+    }
+
 }
