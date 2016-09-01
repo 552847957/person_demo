af293d7e37aaa6cee2f1add1c47904e371a01e32
sunhaidi <sun_hd3@163.com>
2016-09-01 19:50:24 +0800

    活动

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/user/impl/HealthActivityInfoServiceImpl.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/user/impl/HealthActivityInfoServiceImpl.java
@@ -54,12 +54,12 @@ public class HealthActivityInfoServiceImpl implements HealthActivityInfoService 
     @Override
     public List<HealthActivityInfo> getHealthActivityInfos(String province,String city, String county, Integer status, int pageNo, int pageSize) {
 
-        String sql = "select * from app_tb_healthactivity_info where del_flag = '0' and online_status <> 2 ";
+        String sql = "select * from app_tb_healthactivity_info where del_flag = '0' ";
         if(!StringUtils.isEmpty(status)){
             if(status == 1){//活动进行中
-                sql += " and endtime >= now() ";
+                sql += "and starttime <= now() and endtime >= now() and online_status = 1 ";
             }else if(status == 2){//活动已结束
-                sql += " and endtime < now()";
+                sql += " and endtime < now() and online_status = 2";
             }
         }
         if(!StringUtils.isEmpty(province)){
