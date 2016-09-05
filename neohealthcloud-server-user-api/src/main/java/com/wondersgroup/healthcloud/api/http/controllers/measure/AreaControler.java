4a01b89257db3a4d69488c37030d24e3f89e1fcd
zhangzhixiu <zhixiu1018@gmail.com>
2016-09-05 16:20:13 +0800

    update.

--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/AreaControler.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/AreaControler.java
@@ -1,11 +1,14 @@
 package com.wondersgroup.healthcloud.api.http.controllers.measure;
 
+import com.wondersgroup.healthcloud.common.http.annotations.WithoutToken;
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
+import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +18,7 @@ import java.util.Map;
  * Created by Jeffrey on 16/8/29.
  */
 @RestController
+@RequestMapping("/api")
 public class AreaControler {
 
     @Value("${internal.api.service.measure.url}")
@@ -24,7 +28,9 @@ public class AreaControler {
 
     private RestTemplate template = new RestTemplate();
 
-    @GetMapping(value = "api/exam/area/dictionary", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/exam/area/dictionary", produces = MediaType.APPLICATION_JSON_VALUE)
+    @WithoutToken
+    @VersionRange
     public JsonResponseEntity areaDictionary() {
         ResponseEntity<Map> response = template.getForEntity(String.format(requestAreaDictionaryPath, host), Map.class);
         if (response.getStatusCode().equals(HttpStatus.OK)) {
