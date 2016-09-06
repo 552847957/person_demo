4222dfb4c805b01782b1f4eb0a548d753b6ab772
sunhaidi <sun_hd3@163.com>
2016-09-06 19:44:20 +0800

    活动

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/user/dto/healthactivity/HealthActivityAPIEntity.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/user/dto/healthactivity/HealthActivityAPIEntity.java
@@ -1,8 +1,8 @@
 package com.wondersgroup.healthcloud.services.user.dto.healthactivity;
 
-
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -12,111 +12,112 @@ import com.wondersgroup.healthcloud.jpa.entity.activity.HealthActivityInfo;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HealthActivityAPIEntity {
-    private String id;//活动id
-    private String host;// 主办方
-    private String name; // '主题',
-    private String time;// '开始时间',
-    private String location;// '举办地点',
-    private Integer totalAvailable;// '名额',
-    private Integer totalApplied;// 报名人数
-    private String isApplied; // 是否已报名（按钮状态）
-    private String speechMaker;// '主讲人信息 姓名 科室 职务',
-    private String department;//科室
-    private String pftitle;//职称
-    private Integer style;// 讲座形式
-    private String description;// '活动概述',
-    private String picture; // '活动图片存入attach表',
-    private String thumbnail;//活动缩略图
-    private Double score;// 积分
-    private String isEvaluation;//是否可以评价 0:不能评价，1：能评价
-    private String isSurvey;//是否做过满意度调查 0:未做调查，1：已做调查
-    private String contentUrl;//活动内容url地址
-    private String overdue;//活动是否过期：0为未过期，1为过期
-    private String enrollOverdue;//报名时间是否过期：0为未过期，1为过期
-    private boolean ltDay; //再报名时间||活动时间未过期的情况下，剩余时间是否只剩一天
-    private String enrollCountdown;//报名倒计时
-    private String enrollColor;//报名倒计时字体显示颜色
-    
+    private String                            id;                                                            //活动id
+    private String                            host;                                                          // 主办方
+    private String                            name;                                                         // '主题',
+    private String                            time;                                                          // '开始时间',
+    private String                            location;                                                      // '举办地点',
+    private Integer                           totalAvailable;                                                // '名额',
+    private Integer                           totalApplied;                                                  // 报名人数
+    private String                            isApplied;                                                    // 是否已报名（按钮状态）
+    private String                            speechMaker;                                                   // '主讲人信息 姓名 科室 职务',
+    private String                            department;                                                    //科室
+    private String                            pftitle;                                                       //职称
+    private Integer                           style;                                                         // 讲座形式
+    private String                            description;                                                   // '活动概述',
+    private String                            picture;                                                      // '活动图片存入attach表',
+    private String                            thumbnail;                                                     //活动缩略图
+    private Double                            score;                                                         // 积分
+    private String                            isEvaluation;                                                  //是否可以评价 0:不能评价，1：能评价
+    private String                            isSurvey;                                                      //是否做过满意度调查 0:未做调查，1：已做调查
+    private String                            contentUrl;                                                    //活动内容url地址
+    private String                            overdue;                                                       //活动是否过期：0为未过期，1为过期
+    private String                            enrollOverdue;                                                 //报名时间是否过期：0为未过期，1为过期
+    private boolean                           ltDay;                                                        //再报名时间||活动时间未过期的情况下，剩余时间是否只剩一天
+    private String                            enrollCountdown;                                               //报名倒计时
+    private String                            enrollColor;                                                   //报名倒计时字体显示颜色
+
     private HealthActivityEvaluationAPIEntity evaluation;
-    private SimpleDateFormat monthDayStr_sdf = new SimpleDateFormat("MM月dd日");
-    private SimpleDateFormat hourMinute_sdf = new SimpleDateFormat("HH:mm");
-    private SimpleDateFormat time_adf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    
-    private String              starttime;                               // '开始时间',
-    private String              endtime;                                 // '结束时间',
-    private String              onlineTime;                              //上线时间
-    private String              offlineTime;                             //下线时间
-    private String              enrollStartTime;                         //活动报名时间'
-    private String              enrollEndTime;                           //活动结束时间
-    
-    private String partakeActivityDesc;//离参与活动时间最近的一条信息
-    private String partakeActivityId;//离参与活动时间最近的一条id
-    
-    public HealthActivityAPIEntity(HealthActivityInfo info,String width,String height){
-        init(info  ,"activityList",width,height);
+    private SimpleDateFormat                  monthDayStr_sdf = new SimpleDateFormat("MM月dd日");
+    private SimpleDateFormat                  hourMinute_sdf  = new SimpleDateFormat("HH:mm");
+    private SimpleDateFormat                  time_adf        = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private String                            starttime;                                                    // '开始时间',
+    private String                            endtime;                                                      // '结束时间',
+    private String                            onlineTime;                                                   //上线时间
+    private String                            offlineTime;                                                  //下线时间
+    private String                            enrollStartTime;                                              //活动报名时间'
+    private String                            enrollEndTime;                                                //活动结束时间
+
+    private String                            partakeActivityDesc;                                           //离参与活动时间最近的一条信息
+    private String                            partakeActivityId;                                             //离参与活动时间最近的一条id
+
+    public HealthActivityAPIEntity(HealthActivityInfo info, String width, String height) {
+        init(info, "activityList", width, height);
     }
-    
-    public HealthActivityAPIEntity(HealthActivityInfo info , HealthActivityDetail detail , String pageType,String width,String height){
-        init(info  ,pageType,width,height);
+
+    public HealthActivityAPIEntity(HealthActivityInfo info, HealthActivityDetail detail, String pageType, String width,
+            String height) {
+        init(info, pageType, width, height);
     }
-    
-    public void init(HealthActivityInfo info,String pageType,String width,String height){
+
+    public void init(HealthActivityInfo info, String pageType, String width, String height) {
         Timestamp nowTime = new Timestamp(System.currentTimeMillis());
         this.id = info.getActivityid();
         this.host = info.getHost();
         this.name = info.getTitle();
-        
+
         String startHourMin = hourMinute_sdf.format(info.getStarttime());
         String endHourMin = hourMinute_sdf.format(info.getEndtime());
-        
+
         this.overdue = info.getEndtime().getTime() < new Date().getTime() ? "1" : "0";
         this.enrollOverdue = info.getEnrollEndTime().getTime() < new Date().getTime() ? "1" : "0";
-        if("0".equals(this.enrollOverdue)){
+        if ("0".equals(this.enrollOverdue)) {
             this.ltDay = (info.getEnrollEndTime().getTime() - new Date().getTime()) < 86400000;
             this.enrollCountdown = getDateTimeStr(info);
         }
-        if("activityMine".equals(pageType)){//我参与的活动
-            this.time= monthDayStr_sdf.format(info.getStarttime())
-                    + "/"+startHourMin+(startHourMin.equals(endHourMin)?"":"～"+endHourMin);
-        }else{//活动列表、活动详情
-            this.time= monthDayStr_sdf.format(info.getStarttime())
-                    + " "+startHourMin+(startHourMin.equals(endHourMin)?"":"～"+endHourMin);
-        } 
+        if ("activityMine".equals(pageType)) {//我参与的活动
+            this.time = monthDayStr_sdf.format(info.getStarttime()) + "/" + startHourMin
+                    + (startHourMin.equals(endHourMin) ? "" : "～" + endHourMin);
+        } else {//活动列表、活动详情
+            this.time = monthDayStr_sdf.format(info.getStarttime())
+                    + (isSameDate(info.getStarttime(), info.getEndtime()) ? "" : "-"
+                            + monthDayStr_sdf.format(info.getEndtime())) + " " + startHourMin
+                    + (startHourMin.equals(endHourMin) ? "" : "～" + endHourMin);
+        }
         this.totalAvailable = info.getQuota();
 
-        
         this.speechMaker = info.getSpeaker();
         this.style = info.getStyle();
         this.description = info.getSummary();
         this.picture = info.getPhoto();
-        this.thumbnail = info.getThumbnail()+ImagePath.thumbnailPostfix(Integer.parseInt(width)/3+"",Integer.parseInt(width)/3+"");
+        this.thumbnail = info.getThumbnail()
+                + ImagePath.thumbnailPostfix(Integer.parseInt(width) / 3 + "", Integer.parseInt(width) / 3 + "");
 
-
-        if (info.getStarttime().before(nowTime)
-                && info.getEndtime().before(nowTime)) {
+        if (info.getStarttime().before(nowTime) && info.getEndtime().before(nowTime)) {
             this.isEvaluation = "1";// 是否可以评价 0:不能评价，1：能评价
         } else {
             this.isEvaluation = "0";
         }
-        if(info.getStarttime() != null){ 
+        if (info.getStarttime() != null) {
             this.starttime = time_adf.format(info.getStarttime());
         }
-        if(info.getEndtime() != null){ 
+        if (info.getEndtime() != null) {
             this.endtime = time_adf.format(info.getEndtime());
         }
-        if(info.getOnlineTime() != null){ 
+        if (info.getOnlineTime() != null) {
             this.onlineTime = time_adf.format(info.getOnlineTime());
         }
-        if(info.getOfflineTime() != null){ 
+        if (info.getOfflineTime() != null) {
             this.offlineTime = time_adf.format(info.getOfflineTime());
         }
-        if(info.getEnrollStartTime() != null){ 
+        if (info.getEnrollStartTime() != null) {
             this.enrollStartTime = time_adf.format(info.getEnrollStartTime());
         }
-        if(info.getEnrollEndTime() != null){ 
+        if (info.getEnrollEndTime() != null) {
             this.enrollEndTime = time_adf.format(info.getEnrollEndTime());
         }
-        if("0".equals(enrollOverdue)){
+        if ("0".equals(enrollOverdue)) {
             this.enrollCountdown = getDateTimeStr(info);
         }
         this.enrollColor = ltDay ? "#CC0000" : "#666666";
@@ -361,7 +362,7 @@ public class HealthActivityAPIEntity {
     public void setEnrollCountdown(String enrollCountdown) {
         this.enrollCountdown = enrollCountdown;
     }
-    
+
     public String getEnrollColor() {
         return enrollColor;
     }
@@ -386,7 +387,7 @@ public class HealthActivityAPIEntity {
         this.partakeActivityId = partakeActivityId;
     }
 
-    public String getDateTimeStr(HealthActivityInfo info){
+    public String getDateTimeStr(HealthActivityInfo info) {
         long quot = 0;
         quot = info.getEnrollEndTime().getTime() - new Date().getTime();
         quot = quot / 1000 / 60;
@@ -394,15 +395,28 @@ public class HealthActivityAPIEntity {
 
         String str = "";
         long a = quot;
-        long day = a/(60*24);
+        long day = a / (60 * 24);
         a = a - (day * (60 * 24));
         str = str + (String.valueOf(day).length() > 1 ? day : "0" + day) + ":";
-        
-        long hour = a/(60);
-        
+
+        long hour = a / (60);
+
         a = a - (hour * 60);
         str = str + (String.valueOf(hour).length() > 1 ? hour : "0" + hour) + ":";
         str = str + (String.valueOf(a).length() > 1 ? a : "0" + a);
         return str;
+    }
+
+    private static boolean isSameDate(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        
+        boolean isSameYear = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);
+        boolean isSameMonth = isSameYear && cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH);
+        boolean isSameDate = isSameMonth && cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
+
+        return isSameDate;
     }
 }
