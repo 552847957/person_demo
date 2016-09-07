cff058dead8c33ece53c47ab5d758a883d87b4b5
zhangzhixiu <zhixiu1018@gmail.com>
2016-09-07 15:24:20 +0800

    fix root message api.

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/user/UserPrivateMessageRepository.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/user/UserPrivateMessageRepository.java
@@ -22,7 +22,7 @@ import java.util.List;
  */
 public interface UserPrivateMessageRepository extends JpaRepository<UserPrivateMessage, String> {
 
-    @Query(nativeQuery = true, value = "select *,max(create_time) from app_tb_user_private_message where uid=?2 and (main_area is null or main_area=?1) group by type order by create_time desc")
+    @Query(nativeQuery = true, value = "select m.* from app_tb_user_private_message m inner join(select type, max(create_time) as maxtime from app_tb_user_private_message where uid=?2  and (main_area is null or main_area=?1) group by type) g on m.type=g.type and m.create_time=g.maxtime where uid=?2 and (main_area is null or main_area=?1)")
     List<UserPrivateMessage> findRootMessages(String area, String uid);
 
     @Query(nativeQuery = true, value = "select * from app_tb_user_private_message where uid=?2 and type=?3 and create_time<FROM_UNIXTIME(?4) and (main_area is null or main_area=?1) order by create_time desc limit 11")
