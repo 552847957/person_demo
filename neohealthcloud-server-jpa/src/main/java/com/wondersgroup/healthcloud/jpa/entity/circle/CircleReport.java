b6ffaaedd059085ef64064df971ece8b4f1cb447
sunhaidi <sun_hd3@163.com>
2016-09-08 17:10:16 +0800

    设置 int 类型id 自增能涨

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/entity/circle/CircleReport.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/entity/circle/CircleReport.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -32,8 +33,7 @@ public class CircleReport {
     private String dealstatus;
     
     @Id
-    @GeneratedValue()
-//    @GenericGenerator()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "del_flag")
     private String delFlag;
