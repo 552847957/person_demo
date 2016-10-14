864c67c77dbfcc15101db1bf0f629909a0ed7723
zhaozhenxing <zhaozhenxing@wondersgroup.com>
2016-10-14 11:17:35 +0800

    账号登录限制

--- neohealthcloud-server-common-web/src/main/java/com/wondersgroup/healthcloud/common/http/filters/interceptor/InternalRequestHeaderInterceptor.java
+++ neohealthcloud-server-common-web/src/main/java/com/wondersgroup/healthcloud/common/http/filters/interceptor/InternalRequestHeaderInterceptor.java
@@ -36,7 +36,7 @@ public final class InternalRequestHeaderInterceptor extends AbstractHeaderInterc
         }
 
         String reqURI = request.getRequestURI();
-        if(!reqURI.startsWith("/api/login;") && !reqURI.startsWith("/api/welcome;")){
+        if(!reqURI.contains("/api/login;") && !reqURI.contains("/api/welcome;")){
             HeaderCode mainArea = new HeaderCode("main-area", -1000);
             HeaderCode userId = new HeaderCode("userId", -1000);
             if (!checkOne(request, mainArea) || !checkOne(request, userId)) {
