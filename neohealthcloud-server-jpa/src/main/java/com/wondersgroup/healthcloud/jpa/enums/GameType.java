3b6462bf39330ab0c71022f6f8564fc6131802c3
zhuchunliu <zhuchunliu@wondersgroup.com>
2016-10-25 15:22:08 +0800

    导出奖品列表

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/enums/GameType.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/enums/GameType.java
@@ -4,8 +4,15 @@ package com.wondersgroup.healthcloud.jpa.enums;
  * Created by zhuchunliu on 2016/10/21.
  */
 public enum  GameType {
-    bacteria,
-    turntable;
+    BACTERIA("细菌大作战","bacteria"),
+    TURNTABLE("幸运大转盘","turntable");
+    public final String name;
+    public final String type;
+
+    private GameType(String name ,String type){
+        this.name = name;
+        this.type = type;
+    }
 
 
 }
