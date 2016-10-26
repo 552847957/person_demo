224021547bbd6a2c2bfd15608f26f77dd4f8ad7b
zhuchunliu <zhuchunliu@wondersgroup.com>
2016-10-26 10:06:42 +0800

    微信点亮活动

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/enums/GameType.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/enums/GameType.java
@@ -5,7 +5,8 @@ package com.wondersgroup.healthcloud.jpa.enums;
  */
 public enum  GameType {
     BACTERIA("细菌大作战","bacteria"),
-    TURNTABLE("幸运大转盘","turntable");
+    TURNTABLE("幸运大转盘","turntable"),
+    LIGHT("健康点亮活动","light");
     public final String name;
     public final String type;
 
