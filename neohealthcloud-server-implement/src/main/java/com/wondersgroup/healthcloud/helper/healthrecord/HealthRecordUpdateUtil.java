b063cc51671308046bd421be5df7a2a9130d9b63
zhangzhixiu <zhixiu1018@gmail.com>
2016-12-12 14:24:03 +0800

    update.

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/healthrecord/HealthRecordUpdateUtil.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/healthrecord/HealthRecordUpdateUtil.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.lang.model.element.Name;
-
 /**
  * ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
  * ░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
@@ -32,13 +30,17 @@ public class HealthRecordUpdateUtil {//async callback to improve
     @Autowired
     private HttpRequestExecutorManager httpRequestExecutorManager;
 
-    public void onVerificationSuccess(String idcard,String name) {
-        Request request = new RequestBuilder().post().url(host + "/api/healthRecord/updateRecord").body(String.format("{\"idc\":\"%s\",\"name\":\"%s\"}", idcard,name)).build();
+    public void onVerificationSuccess(String idcard, String name) {
+        Request request = new RequestBuilder().post().url(host + "/api/healthRecord/updateRecord").body(String.format("{\"idc\":\"%s\",\"name\":\"%s\"}", idcard, name)).build();
         httpRequestExecutorManager.newCall(request).run();
     }
 
     public void onMedicareBindSuccess(String idcard, String medicareCard, String name) {
         Request request = new RequestBuilder().post().url(host + "/api/healthRecord/bindMedicarecard").body(String.format("{\"idc\":\"%s\",\"medicareCard\":\"%s\",\"name\":\"%s\"}", idcard, medicareCard, name)).build();
         httpRequestExecutorManager.newCall(request).run();
+    }
+
+    public void unBindMedicareCard(String idcard) {
+        //todo
     }
 }
