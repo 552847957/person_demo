6c33e550ddf37d4f2abd5e65293bb6874b575c87
zhongshuqing <zhongshuqing@wondersgroup.com>
2016-12-14 20:19:35 +0800

    话题push fix

--- neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/utils/AppUrlSchemaUtils.java
+++ neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/utils/AppUrlSchemaUtils.java
@@ -9,8 +9,10 @@ package com.wondersgroup.healthcloud.common.utils;
 public class AppUrlSchemaUtils {
 
     public static String basePath = "com.wondersgroup.healthcloud.${area}://user/";
-
-    private static String getBasePath(String area){
+    
+    public static String areaCode="3031";
+    
+    public static String getBasePath(String area){
         return basePath.replace("${area}", area);
     }
 
@@ -18,7 +20,7 @@ public class AppUrlSchemaUtils {
      * 圈子的话题详情
      */
     public static String bbsTopicView(int topic_id) {
-        return getBasePath("3031") + "bbs/topic_detail?topic_id="+topic_id;
+        return getBasePath(areaCode) + "bbs/topic_detail?topic_id="+topic_id;
     }
 
 
--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/push/plan/PushPlanService.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/helper/push/plan/PushPlanService.java
@@ -1,31 +1,46 @@
 package com.wondersgroup.healthcloud.helper.push.plan;
 
-import com.google.common.collect.Lists;
-import com.wondersgroup.healthcloud.jpa.entity.permission.User;
-import com.wondersgroup.healthcloud.jpa.entity.push.PushPlan;
-import com.wondersgroup.healthcloud.jpa.repository.permission.UserRepository;
-import com.wondersgroup.healthcloud.jpa.repository.push.PushPlanRepository;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.apache.commons.lang3.StringUtils;
-import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.*;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Lists;
+import com.squareup.okhttp.Request;
+import com.wondersgroup.common.http.HttpRequestExecutorManager;
+import com.wondersgroup.common.http.builder.RequestBuilder;
+import com.wondersgroup.common.http.entity.JsonNodeResponseWrapper;
+import com.wondersgroup.healthcloud.jpa.constant.AppPushConstant;
+import com.wondersgroup.healthcloud.jpa.entity.permission.User;
+import com.wondersgroup.healthcloud.jpa.entity.push.PushPlan;
+import com.wondersgroup.healthcloud.jpa.repository.permission.UserRepository;
+import com.wondersgroup.healthcloud.jpa.repository.push.PushPlanRepository;
 
 /**
  * Created by zhuchunliu on 2016/8/27.
  */
 @Service
 public class PushPlanService {
-
+    private Logger logger = LoggerFactory.getLogger(PushPlanService.class);
+    @Autowired
+    private HttpRequestExecutorManager httpRequestExecutorManager;
+    
     @Autowired
     private PushPlanRepository pushPlanRepo;
 
@@ -69,6 +84,29 @@ public class PushPlanService {
 
         return pushPlanRepo.findAll(specification,pageable);
     }
-
+    
+    @Transactional
+    public String cancel(Integer id,Integer preStatus,String jobClientUrl,String topicUrl,Integer status){
+        PushPlan pushPlan = pushPlanRepo.findOne(id);
+        if((1 == status || 4 == status) && 0 != pushPlan.getStatus()){//通过
+            return "问题非待审核状态";
+        }
+        pushPlan.setStatus(status);
+        pushPlan.setUpdateTime(new Date());
+        pushPlanRepo.save(pushPlan);
+        //取消定时任务
+        if(1 == preStatus) {//之前状态为待推送状态，则可以取消定时任务
+            Request build=null;
+            if(pushPlan.getType()==AppPushConstant.PushType.ARTICLE){
+                build = new RequestBuilder().delete().url(jobClientUrl + "/api/healthcloud/push").param("planId", id.toString()).build();
+            }else if(pushPlan.getType()==AppPushConstant.PushType.TOPIC){
+                build = new RequestBuilder().delete().url(topicUrl + "/api/healthcloud/push").param("planId", id.toString()).build();
+            }
+            JsonNodeResponseWrapper response = (JsonNodeResponseWrapper)httpRequestExecutorManager.newCall(build).run().as(JsonNodeResponseWrapper.class);
+            JsonNode result = response.convertBody();
+            logger.error("定时任务(pushId = "+id+")取消成功，返回结果"+result);
+    }
+     return "取消成功";   
+}
 
 }
