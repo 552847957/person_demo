ad280c9f596812f5d3c691e88d80cafbdd516dc1
dukuanxin <dukuanxin@wondersgroup.com>
2016-12-15 11:39:39 +0800

    个人中心添加预约挂号开关

--- neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/user/UserCenterController.java
+++ neohealthcloud-server-user-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/user/UserCenterController.java
@@ -2,13 +2,11 @@ package com.wondersgroup.healthcloud.api.http.controllers.user;
 
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
+import com.wondersgroup.healthcloud.services.appointment.AppointmentApiService;
 import com.wondersgroup.healthcloud.services.bbs.UserBbsService;
 import com.wondersgroup.healthcloud.services.bbs.UserFansService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +25,8 @@ public class UserCenterController {
     @Autowired
     private UserBbsService userBbsService;
 
+    @Autowired
+    private AppointmentApiService appointmentApiService;
     /**
      * 个人中心首页接口
      * @param uid
@@ -34,17 +34,19 @@ public class UserCenterController {
      */
     @VersionRange
     @GetMapping("/center/home")
-    public JsonResponseEntity userCenterHome(@RequestParam String uid){
+    public JsonResponseEntity userCenterHome(@RequestHeader("main-area") String mainArea, @RequestParam String uid){
         JsonResponseEntity response=new JsonResponseEntity();
         int concernCount=fansService.countAttentNum(uid);//我的关注数
         int fansCount=fansService.countFansNum(uid);//我的粉丝数
         int topicCount=userBbsService.countTopicByUid(uid);//我发表的话题数
         int replyCount=userBbsService.countCommentByUid(uid);//我的回复数
+        Boolean registrationIsOn = appointmentApiService.getRegistrationIsOn(mainArea);
         Map data=new HashMap<>();
         data.put("concernCount",concernCount);
         data.put("fansCount",fansCount);
         data.put("topicCount",topicCount);
         data.put("replyCount",replyCount);
+        data.put("registrationIsOn",registrationIsOn);
         response.setData(data);
         return response;
     }
