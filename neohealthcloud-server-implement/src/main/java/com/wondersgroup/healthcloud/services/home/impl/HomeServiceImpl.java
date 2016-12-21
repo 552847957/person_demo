66bb7178f8d538059bb800f5507f0c5da52495e4
xianglinhai <xianglinhai@wondersgroup.com>
2016-12-21 20:33:42 +0800

    家人通知提醒，异常显示内容

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/home/impl/HomeServiceImpl.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/home/impl/HomeServiceImpl.java
@@ -680,7 +680,7 @@ public class HomeServiceImpl implements HomeService {
             for (UserHealthItemDTO dto : familyMemberHealth.getExceptionItems()) {
                 ftemDTO = new FamilyMemberItemDTO();
                 ftemDTO.setRelationship(FamilyMemberRelation.getName(fm.getRelation()));
-                ftemDTO.setPrompt(dto.getData() + " " + (dto.getHightAndLow().equals("1") ? "偏高" : "偏低"));
+                ftemDTO.setPrompt(dto.getName() +  (dto.getHightAndLow().equals("1") ? "偏高" : "偏低"));
                 break;//家人有多项异常，只取一项
             }
         }
