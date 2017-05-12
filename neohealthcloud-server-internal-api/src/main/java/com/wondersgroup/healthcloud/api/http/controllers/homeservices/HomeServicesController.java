eff16d57b7bf5d39c662aafb9a510009eb812257
xianglinhai@wondersgroup.com <xianglinhai@wondersgroup.com>
2017-05-12 18:15:20 +0800

    后台接口完善

--- neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/homeservices/HomeServicesController.java
+++ neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/homeservices/HomeServicesController.java
@@ -1,5 +1,7 @@
 package com.wondersgroup.healthcloud.api.http.controllers.homeservices;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.wondersgroup.healthcloud.api.http.dto.cloudtopline.CloudTopLineViewDTO;
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
 import com.wondersgroup.healthcloud.common.http.support.misc.JsonKeyReader;
@@ -11,6 +13,7 @@ import com.wondersgroup.healthcloud.jpa.entity.cloudtopline.CloudTopLine;
 import com.wondersgroup.healthcloud.jpa.entity.config.AppConfig;
 import com.wondersgroup.healthcloud.jpa.entity.homeservice.HomeServiceEntity;
 import com.wondersgroup.healthcloud.jpa.enums.CloudTopLineEnum;
+import com.wondersgroup.healthcloud.jpa.enums.ServiceTypeEnum;
 import com.wondersgroup.healthcloud.jpa.repository.bbs.TopicRepository;
 import com.wondersgroup.healthcloud.jpa.repository.cloudtopline.CloudTopLineRepository;
 import com.wondersgroup.healthcloud.jpa.repository.homeservice.HomeUserServiceRepository;
@@ -18,6 +21,7 @@ import com.wondersgroup.healthcloud.services.article.ManageNewsArticleService;
 import com.wondersgroup.healthcloud.services.cloudTopLine.CloudTopLineService;
 import com.wondersgroup.healthcloud.services.config.AppConfigService;
 import com.wondersgroup.healthcloud.services.homeservice.HomeServices;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
@@ -72,6 +76,17 @@ public class HomeServicesController  {
     }
 
     @VersionRange
+    @RequestMapping(value = "/manage/getServiceType", method = RequestMethod.GET)
+    public Object getServiceType(){
+               List<ServiceType> serviceType = new ArrayList<ServiceType>();
+                for(ServiceTypeEnum ste:ServiceTypeEnum.values()){
+                    serviceType.add(new ServiceType(ste.getType(),ste.getName()));
+                }
+
+        return  new JsonResponseEntity(0, "操作成功!",serviceType);
+    }
+
+    @VersionRange
     @RequestMapping(value = "/manage/editHomeServices", method = RequestMethod.POST)
     public Object addCloudTopLine(@RequestBody(required = true) String body) {
         JsonResponseEntity entity = new JsonResponseEntity();
@@ -80,4 +95,14 @@ public class HomeServicesController  {
         return  new JsonResponseEntity(0, "操作成功!");
     }
 
+}
+
+@Data
+@JsonNaming
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class ServiceType{
+    public ServiceType (){}
+    public ServiceType(int id,String name){this.id = id;this.name = name;}
+      private int id;
+      private String name;
 }
