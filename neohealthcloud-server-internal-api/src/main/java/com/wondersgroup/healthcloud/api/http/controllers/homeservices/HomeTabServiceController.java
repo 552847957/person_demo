332bbc49f8e09b3288577a4460e2191ecf1d12a9
xianglinhai@wondersgroup.com <xianglinhai@wondersgroup.com>
2017-05-16 17:53:33 +0800

    底部tab编辑修改

--- neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/homeservices/HomeTabServiceController.java
+++ neohealthcloud-server-internal-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/homeservices/HomeTabServiceController.java
@@ -2,10 +2,7 @@ package com.wondersgroup.healthcloud.api.http.controllers.homeservices;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.wondersgroup.healthcloud.common.http.dto.JsonResponseEntity;
 import com.wondersgroup.healthcloud.common.http.support.version.VersionRange;
 import com.wondersgroup.healthcloud.jpa.entity.homeservice.HomeTabServiceEntity;
@@ -85,33 +82,32 @@ public class HomeTabServiceController {
 
     }
 
-    @VersionRange
-    @RequestMapping(value = "/manage/editTabHomeService", method = RequestMethod.POST)
-    public Object editTabHomeService(@RequestBody(required = true) String body) {
-        Gson gson = new Gson();
-        JsonParser parser = new JsonParser();
-        JsonArray array = parser.parse(body).getAsJsonArray();
-        for (int i = 0; i < array.size(); i++) {
-            JsonObject subObject = array.get(i).getAsJsonObject();//获取数组的值并转化为JsonObjecty格式
-            HomeTabServiceEntity entity = gson.fromJson(subObject, HomeTabServiceEntity.class);
+    public JsonResponseEntity checkObject(List<HomeTabServiceEntity> list){
 
-            if(null == entity){
-                return new JsonResponseEntity(-1, "內部錯誤，数据解析异常!");
-            }
+      for(HomeTabServiceEntity entity:list){
+        /*if(null == entity){
+            return new JsonResponseEntity(-1, "內部錯誤，数据解析异常!");
+        }*/
 
-            if(StringUtils.isBlank(entity.getVersion())){
-                return new JsonResponseEntity(-1, "版本号不能为空!");
-            }
+        if(StringUtils.isBlank(entity.getVersion())){
+            return new JsonResponseEntity(-1, "版本号不能为空!");
+        }
 
-            if(!entity.getVersion().matches("\\d+\\.\\d+\\.\\d+")){
-                return new JsonResponseEntity(-1, "版本号不满足规则!");
-            }
+        if(!entity.getVersion().matches("\\d+\\.\\d+\\.\\d+")){
+            return new JsonResponseEntity(-1, "版本号不满足规则!");
+        }
 
-            if(StringUtils.isBlank(entity.getImgUrl())){
-                return new JsonResponseEntity(-1," 图片地址不能为空!");
-            }
+        if(StringUtils.isBlank(entity.getImgUrl())){
+            return new JsonResponseEntity(-1," 图片地址不能为空!");
+        }
+     }
+        return null;
+    }
 
+    JsonResponseEntity saveTabService(List<HomeTabServiceEntity> list,TabServiceTypeEnum tb){
+        for( HomeTabServiceEntity entity:list){
 
+            entity.setTabType(tb.getType());
             if (StringUtils.isNotBlank(entity.getId())) { //修改
                 homeServicesImpl.updateHomeTabService(entity);
             } else if (StringUtils.isBlank(entity.getId())) {//新增
@@ -119,6 +115,62 @@ public class HomeTabServiceController {
                 homeServicesImpl.saveHomeTabService(entity);
             }
         }
+
+        return null;
+    }
+
+    List<HomeTabServiceEntity> toEntityList(JsonArray array,String version){
+        Gson gson = new Gson();
+        List<HomeTabServiceEntity> list = new ArrayList<HomeTabServiceEntity>();
+        for (int i = 0; i < array.size(); i++) {
+            JsonObject subObject = array.get(i).getAsJsonObject();//获取数组的值并转化为JsonObjecty格式
+            HomeTabServiceEntity entity = gson.fromJson(subObject, HomeTabServiceEntity.class);
+            entity.setVersion(version);
+            list.add(entity);
+        }
+        return list;
+    }
+
+
+    @VersionRange
+    @RequestMapping(value = "/manage/editTabHomeService", method = RequestMethod.POST)
+    public Object editTabHomeService(@RequestBody(required = true) String body) {
+
+        JsonParser parser = new JsonParser();
+        JsonObject jo = parser.parse(body).getAsJsonObject();
+        JsonElement version = jo.get("version") ;
+         if(null == version){
+             return new JsonResponseEntity(-1, "版本号不能为空!");
+         }
+
+        JsonArray backgroundArray =  jo.getAsJsonArray("background");//背景
+        JsonArray noHighlightArray = jo.getAsJsonArray("noHighlight");//非高亮
+        JsonArray highlightArray = jo.getAsJsonArray("highlight");//高亮
+
+        List<HomeTabServiceEntity> backgroundList =  toEntityList(backgroundArray,version.getAsString());
+        List<HomeTabServiceEntity> noHighlightList = toEntityList(noHighlightArray,version.getAsString());
+        List<HomeTabServiceEntity> highlightList = toEntityList(highlightArray,version.getAsString());
+
+        JsonResponseEntity entity = null;
+        entity = checkObject(backgroundList);
+
+        if(null != entity){
+            return entity;
+        }
+
+        entity = checkObject(noHighlightList);
+        if(null != entity){
+            return entity;
+        }
+
+        entity = checkObject(highlightList);
+        if(null != entity){
+            return entity;
+        }
+
+        saveTabService(backgroundList,TabServiceTypeEnum.BACKGROUND);
+        saveTabService(noHighlightList,TabServiceTypeEnum.NO_HIGHTLIGHT);
+        saveTabService(highlightList,TabServiceTypeEnum.HIGHTLIGHT);
 
         return new JsonResponseEntity(0, "操作成功!");
     }
