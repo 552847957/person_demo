0402c47d4c5f66cc3c8ae7a0d5f4e04fbee7ac5a
xianglinhai@wondersgroup.com <xianglinhai@wondersgroup.com>
2017-05-17 16:10:53 +0800

    代码还原

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/user/DiseaseRegisterInfoRepository.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/user/DiseaseRegisterInfoRepository.java
@@ -1,12 +1,13 @@
 package com.wondersgroup.healthcloud.jpa.repository.user;
 
-import com.wondersgroup.healthcloud.jpa.entity.user.DiseaseRegisterInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.wondersgroup.healthcloud.jpa.entity.user.DiseaseRegisterInfo;
+
 public interface DiseaseRegisterInfoRepository extends JpaRepository<DiseaseRegisterInfo,String> {
 
-    @Query("select a from DiseaseRegisterInfo a where a.registerid=?1 and a.delFlag='0' and a.type='3'")
+    @Query("select a from DiseaseRegisterInfo a where a.registerid=? and a.delFlag='0' and a.type='3'")
     DiseaseRegisterInfo findByUserId(String registerid);
 
 }
