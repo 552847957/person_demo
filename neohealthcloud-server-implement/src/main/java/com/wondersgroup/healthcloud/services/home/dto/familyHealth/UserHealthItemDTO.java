b7587bd003aed296cc52f90f2360c44ab96f406b
zhongshuqing <zhongshuqing@wondersgroup.com>
2017-05-22 17:09:54 +0800

    bugFix

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/home/dto/familyHealth/UserHealthItemDTO.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/home/dto/familyHealth/UserHealthItemDTO.java
@@ -9,7 +9,7 @@ import lombok.Data;
  * 个人健康 异常项
  * Created by xianglihai on 2016/12/13.
  */
-@Data
+
 @JsonNaming
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserHealthItemDTO {
@@ -18,4 +18,38 @@ public class UserHealthItemDTO {
     private String hightAndLow;
     private Long testTime;
     private String testPeriod;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getHightAndLow() {
+        return hightAndLow;
+    }
+    public void setHightAndLow(String hightAndLow) {
+        this.hightAndLow = hightAndLow;
+    }
+    public Long getTestTime() {
+        return testTime;
+    }
+    public void setTestTime(Long testTime) {
+        this.testTime = testTime;
+    }
+    public String getTestPeriod() {
+        if(testPeriod==null){
+            return "";
+        }
+        return testPeriod;
+    }
+    public void setTestPeriod(String testPeriod) {
+        this.testPeriod = testPeriod;
+    }
+    
 }
---
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/entity/group/PatientGroup.java
@@ -0,0 +1,48 @@
+package com.wondersgroup.healthcloud.jpa.entity.group;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+/**
+ * 
+ * @author zhongshuqing 
+ *     患者分组
+ */
+@Entity
+@Data
+@JsonNaming
+@Table(name = "app_tb_patient_group")
+public class PatientGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    
+    private Integer rank=100;//排序
+    
+    @Column(name = "doctor_id")
+    private String doctorId;
+    
+    @Column(name = "del_flag")
+    private String delFlag="0";
+    
+    @Column(name = "update_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    @Column(name = "create_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+}
