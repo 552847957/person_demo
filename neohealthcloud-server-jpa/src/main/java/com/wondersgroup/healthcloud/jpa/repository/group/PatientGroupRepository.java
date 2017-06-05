78ccf496042f244e5606995ab1e43c6cd5734cad
zhongshuqing <zhongshuqing@wondersgroup.com>
2017-06-05 10:17:39 +0800

    bugFix

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/group/PatientGroupRepository.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/group/PatientGroupRepository.java
@@ -12,7 +12,7 @@ public interface PatientGroupRepository extends JpaRepository<PatientGroup, Inte
     @Query(nativeQuery=true,value="SELECT * FROM app_tb_patient_group p WHERE p.doctor_id=?1 AND p.del_flag='0' ORDER BY p.rank ASC")
     List<PatientGroup> getPatientGroupByDoctorId(String doctorId);
     
-    @Query(nativeQuery=true,value="SELECT * FROM app_tb_patient_group p WHERE p.doctor_id=?1 AND p.name=?2")
+    @Query(nativeQuery=true,value="SELECT * FROM app_tb_patient_group p WHERE p.doctor_id=?1 AND p.name=?2 AND p.del_flag='0' ")
     PatientGroup findIsNameRepeated(String doctorId,String name);
     
     @Query(nativeQuery=true,value="SELECT t1.* FROM app_tb_patient_group t1 INNER JOIN app_tb_sign_user_doctor_group t2 ON t1.id=t2.group_id WHERE t1.doctor_id=?1 AND t1.id=?2 AND t2.del_flag='0' AND t1.del_flag='0' ")
