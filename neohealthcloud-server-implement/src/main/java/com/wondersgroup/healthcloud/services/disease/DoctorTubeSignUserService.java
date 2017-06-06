c0b1714d4728f6fc2c2b9cf4f16e6a46683091ca
limenghua <limenghua@wondersgroup.com>
2017-06-06 17:01:48 +0800

    居民列表-添加根据分组查询的接口定义

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/disease/DoctorTubeSignUserService.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/disease/DoctorTubeSignUserService.java
@@ -1,6 +1,7 @@
 package com.wondersgroup.healthcloud.services.disease;
 
 import com.wondersgroup.healthcloud.jpa.entity.diabetes.DoctorTubeSignUser;
+import com.wondersgroup.healthcloud.services.disease.dto.ResidentInfoDto;
 
 import java.util.List;
 
@@ -12,4 +13,12 @@ import java.util.List;
 public interface DoctorTubeSignUserService {
 
     List<DoctorTubeSignUser> search(final DoctorTubeSignUser user, int page);
+
+    /**
+     * @param groupId  分组id
+     * @param page     页码  不传默认第一页
+     * @param pageSize 每页多少条数据,默认100条
+     * @return
+     */
+    List<ResidentInfoDto> queryByGroup(String groupId, int page, int pageSize);
 }
---
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/disease/dto/ResidentInfoDto.java
@@ -0,0 +1,17 @@
+package com.wondersgroup.healthcloud.services.disease.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.wondersgroup.healthcloud.services.doctor.dto.BaseResidentDto;
+import lombok.Data;
+
+/**
+ * Created by limenghua on 2017/6/6.
+ *
+ * @author limenghua
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResidentInfoDto extends BaseResidentDto {
+    //是否分组
+    private Boolean ifGrouped;
+}
