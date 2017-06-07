556f7a5d68fe1556603f34b328981bf8f2542e14
sunhaidi <sun_hd3@163.com>
2017-06-07 10:24:44 +0800

    健康档案信息

--- neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/appenum/ImageTextEnum.java
+++ neohealthcloud-server-common/src/main/java/com/wondersgroup/healthcloud/common/appenum/ImageTextEnum.java
@@ -18,7 +18,8 @@ public enum ImageTextEnum {
 	AD_HOME("个人诊所",10),
     G_HOME_SPECIAL_SERVICE("3.0首页特色服务", 11),// 组图
     MONITOR_PLAN("4.0慢病监测方案", 12),
-    AD_HEALTH_CIRCLE("健康圈",13);
+    AD_HEALTH_CIRCLE("健康圈",13),
+    AD_HEALTH_RECORD("健康档案图标",14);
 
     private String name;
     private Integer type;
