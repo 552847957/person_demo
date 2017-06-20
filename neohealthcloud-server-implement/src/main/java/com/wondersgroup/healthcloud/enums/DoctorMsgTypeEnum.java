a3da79ef8c61103cfd44e04d29e24765e84fa933
longshasha <longshasha@wondersgroup.com>
2017-06-20 14:12:45 +0800

    用户提问追问时给医生添加消息urlFragment添加参数

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/enums/DoctorMsgTypeEnum.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/enums/DoctorMsgTypeEnum.java
@@ -10,7 +10,7 @@ public enum DoctorMsgTypeEnum {
 
     msgTypeSys("1","系统消息",""),
     msgType2("2","医生私信",""),//不知道是不是医生私信(产品说他也不知道医生私信是什么==去掉)
-    msgTypeQuestion("3","问诊消息","com.wondersgroup.healthcloud.3101://doctor/question?questionId=%s"),
+    msgTypeQuestion("3","问诊消息","com.wondersgroup.healthcloud.3101://doctor/question?questionId=%s&type=0"),
     msgTypeInterven("4","干预提醒","com.wondersgroup.healthcloud.3101://doctor/intervention_detail?patientId=%s");//测量项目也有用到
 
 
