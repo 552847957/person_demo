12bdc1081446ab562d3cebb850e003ec9b2bd733
ZZX <zhaozhenxing@wondersgroup.com>
2017-06-21 17:38:33 +0800

    签约用户列表，居民地址bug fix

--- neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/sign/impl/SignServiceImpl.java
+++ neohealthcloud-server-implement/src/main/java/com/wondersgroup/healthcloud/services/sign/impl/SignServiceImpl.java
@@ -157,10 +157,11 @@ public class SignServiceImpl implements SignService {
         // 设置地址信息
         Address address = addressRepository.queryFirst1ByDelFlagAndPersoncard(doctorTubeSignUser.getCardNumber());
         if (address != null) {
-            String adr = String.format("%s%s%s%s%s",
+            String adr = String.format("%s%s%s%s%s%s",
                     StringUtils.trimToEmpty(dictCache.queryArea(address.getProvince())),
                     StringUtils.trimToEmpty(dictCache.queryArea(address.getCity())),
                     StringUtils.trimToEmpty(dictCache.queryArea(address.getCounty())),
+                    StringUtils.trimToEmpty(dictCache.queryArea(address.getTown())),
                     StringUtils.trimToEmpty(dictCache.queryArea(address.getCommittee())),
                     StringUtils.trimToEmpty(address.getOther()));
             dto.setAddress(adr);
