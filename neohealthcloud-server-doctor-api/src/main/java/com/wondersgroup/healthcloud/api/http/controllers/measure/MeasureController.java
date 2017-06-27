b1b8bacc14680fa61d5cceda6a999b0a6a6ed1f4
sunhaidi <sun_hd3@163.com>
2017-06-27 14:23:50 +0800

    风险评估文描

--- neohealthcloud-server-doctor-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
+++ neohealthcloud-server-doctor-api/src/main/java/com/wondersgroup/healthcloud/api/http/controllers/measure/MeasureController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.wondersgroup.healthcloud.api.http.dto.doctor.AssessmentAbnormal;
+import com.wondersgroup.healthcloud.api.http.dto.doctor.disease.ScreeningDto;
 import com.wondersgroup.healthcloud.api.http.dto.doctor.heathrecord.HeathIconDto;
 import com.wondersgroup.healthcloud.api.http.dto.doctor.heathrecord.HeathUserInfoDto;
 import com.wondersgroup.healthcloud.common.http.annotations.JsonEncode;
@@ -21,6 +22,7 @@ import com.wondersgroup.healthcloud.jpa.entity.user.RegisterInfo;
 import com.wondersgroup.healthcloud.jpa.repository.assessment.AssessmentRepository;
 import com.wondersgroup.healthcloud.jpa.repository.diabetes.DoctorTubeSignUserRepository;
 import com.wondersgroup.healthcloud.services.assessment.AssessmentService;
+import com.wondersgroup.healthcloud.services.assessment.dto.AssessmentConstrains;
 import com.wondersgroup.healthcloud.services.imagetext.ImageTextService;
 import com.wondersgroup.healthcloud.services.interven.DoctorIntervenService;
 import com.wondersgroup.healthcloud.services.user.UserService;
@@ -43,6 +45,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -735,7 +738,7 @@ public class MeasureController {
             for (Assessment assessment : list) {
                 AssessmentAbnormal as = new AssessmentAbnormal();
                 as.setDate(new SimpleDateFormat("yyyy-MM-dd").format(assessment.getCreateDate()));
-                as.setCause(cause(assessment, assessmentService));
+                as.setCause(cause(assessment));
                 String result = "";
                 String su = assessmentService.getResult(assessment);
                 String[] split = su.split(",");
@@ -902,67 +905,77 @@ public class MeasureController {
         return url;
     }
 
-    public static List<String> cause(Assessment as, AssessmentService assessmentService){
+    public static List<String> cause(Assessment assessment){
         List<String> list = new ArrayList<String>();
-        if(as == null ){
+        if(assessment == null ){
             return list;
         }
-        if(as.getAge() != null && as.getAge() >= 40){
-            list.add("年龄=" + as.getAge());
-        }if(assessmentService.overWeight(as.getHeight(),as.getWeight())){
-            list.add("超重/肥胖");
-        }if(as.getWaist() != null && (("1".equals(as.getGender()) && as.getWaist() >= 90) || ("2".equals(as.getGender()) && as.getWaist() >= 85))){
-            list.add("中心性肥胖");
-        }if(as.getDiabetesRelatives() != null && !"0".equals(as.getDiabetesRelatives().toString())){
-            list.add("亲属中有糖尿病患者");
-        }if(as.getHypertensionRelatives() != null && !"0".equals(as.getHypertensionRelatives().toString())){
-            list.add("亲属中有高血压患者");
-        }if(as.getStrokeRelatives() != null && !"0".equals(as.getStrokeRelatives().toString())){
-            list.add("亲属中有脑卒中患者");
-        }if(as.getIsDrink() != null && as.getIsDrink() == 2){
-            list.add("经常喝酒");
-        }if(as.getIsDrink() != null && as.getIsDrink() == 3){
-            list.add("每天都喝酒");
-        }if(as.getIsSmoking() != null && as.getIsSmoking() == 1){
-            list.add("现在每天吸烟");
-        }if(as.getIsSmoking() != null && as.getIsSmoking() == 2){
-            list.add("现在吸烟，但不是每天吸烟");
-        }if(as.getIsSmoking() != null && as.getIsSmoking() == 3){
-            list.add("过去吸烟，现在不吸烟");
-        }if(as.getEatHabits() != null && as.getEatHabits() == 2){
-            list.add("饮食习惯荤食为主");
-        }if(as.getEatHabits() != null && as.getEatHabits() == 3){
-            list.add("饮食习惯素食为主");
-        }if(as.getEatTaste() != null && "2".equals(as.getEatTaste())){
-            list.add("饮食口味嗜油");
-        }if(as.getEatTaste() != null && "1".equals(as.getEatTaste())){
-            list.add("饮食口味嗜盐");
-        }if(as.getEatTaste() != null && "3".equals(as.getEatTaste())){
-            list.add("饮食口味嗜糖");
-        }if(as.getSport() != null && as.getSport() == 4){
-            list.add("严重缺乏运动");
-        }if(as.getAge() != null && as.getAge() >= 40){
-            list.add("血压" + as.getPressure() + "mmHg");
-        }if(as.getIsDyslipidemia() != null && as.getIsDyslipidemia() == 1){
-            list.add("血脂异常");
-        }if(as.getMedicalHistory() != null && "1".equals(as.getMedicalHistory())){
-            list.add("有糖调节受损");
-        }if(as.getMedicalHistory() != null && "2".equals(as.getMedicalHistory())){
-            list.add("动脉粥样硬化心脑血管疾病");
-        }if(as.getMedicalHistory() != null && "3".equals(as.getMedicalHistory())){
-            list.add("有过一性类固醇糖尿病");
-        }if(as.getMedicalHistory() != null && "4".equals(as.getMedicalHistory())){
-            list.add("房颤或明显的脉搏不齐");
-        }if(as.getMedicalHistory() != null && "5".equals(as.getMedicalHistory())){
-            list.add("短暂脑缺血发作病史");
-        }if(as.getIsDepression() != null && as.getIsDepression() == 1){
-            list.add("长期接受抗精神类药物");
-        }if(as.getFemaleMedicalHistory() != null && "1".equals(as.getFemaleMedicalHistory())){
-            list.add("有巨大儿");
-        }if(as.getFemaleMedicalHistory() != null && "2".equals(as.getFemaleMedicalHistory())){
-            list.add("有妊娠期糖尿病史");
-        }if(as.getFemaleMedicalHistory() != null && "3".equals(as.getFemaleMedicalHistory())){
-            list.add("多囊卵巢综合症患者");
+        if(assessment.getAge()>=40){
+            list.add("年龄="+assessment.getAge());
+        }
+
+        Double bmi = Double.valueOf(new DecimalFormat("##.00").
+                format(Double.valueOf(assessment.getWeight())/Math.pow((Double.valueOf(assessment.getHeight())/100), 2)));
+        if(bmi>=24){
+            list.add( "超重/肥胖");
+        }
+
+        if (null != assessment.getWaist() && ((assessment.getGender().equals(AssessmentConstrains.GENDER_MAN) && assessment.getWaist() >= 90 )||
+                (assessment.getGender().equals(AssessmentConstrains.GENDER_WOMAN) && assessment.getWaist() >= 85 ))) {
+            list.add( "中心性肥胖");
+        }
+        if(!"0".equals(assessment.getDiabetesRelatives())){
+            list.add( "亲属中有糖尿病患者");
+        }
+        if(!"0".equals(assessment.getHypertensionRelatives())){
+            list.add( "亲属中有高血压患者");
+        }
+        if(!"0".equals(assessment.getStrokeRelatives())){
+            list.add( "亲属中有脑卒中患者");
+        }
+        if(3 == assessment.getIsDrink()){
+            list.add( "每天都喝酒");
+        }
+        if(1 == assessment.getIsSmoking() || 2 == assessment.getIsSmoking()){
+            list.add(1 == assessment.getIsSmoking() ? "现在每天吸烟" : "现在吸烟，但不是每天吸烟");
+        }
+        if(1 != assessment.getEatHabits()){
+            list.add( 2 == assessment.getEatHabits()?"饮食习惯荤食为主":"饮食习惯素食为主");
+        }
+        if(!"4".equals(assessment.getEatTaste())){
+            if(assessment.getEatTaste().contains("1")) list.add( "饮食口味嗜盐");
+            if(assessment.getEatTaste().contains("2")) list.add( "饮食口味嗜油");
+            if(assessment.getEatTaste().contains("3")) list.add( "饮食口味嗜糖");
+        }
+        if(3 == assessment.getSport() || 4 == assessment.getSport()){
+            list.add( "严重缺乏运动");
+        }
+        String[] pressures = assessment.getPressure().split("/");
+        Integer diastolic = Integer.valueOf(pressures[1]);
+        Integer systolic = Integer.valueOf(pressures[0]);
+        if(diastolic >=85 || systolic >=130){
+            list.add( "血压"+assessment.getPressure()+"mmHg");
+        }
+        if(1 == assessment.getTakeAntihypertensiveDrugs()){
+            list.add( "正在服用降压药");
+        }
+        if(1 == assessment.getIsDyslipidemia()){
+            list.add( "血脂异常");
+        }
+        if(!"6".equals(assessment.getMedicalHistory())){
+            if(assessment.getMedicalHistory().contains("1")) list.add( "有糖调节受损（IGR，又称糖尿病前期)");
+            if(assessment.getMedicalHistory().contains("2")) list.add( "动脉粥样硬化心脑血管疾病");
+            if(assessment.getMedicalHistory().contains("3")) list.add( "有一过性类固醇糖尿病");
+            if(assessment.getMedicalHistory().contains("4")) list.add( "房颤或明显的脉搏不齐");
+            if(assessment.getMedicalHistory().contains("5")) list.add( "短暂脑缺血发作病史（TIA）");
+        }
+        if(1 == assessment.getIsDepression()){
+            list.add( "长期接受抗精神类药物（或）抗抑郁症药物治疗");
+        }
+        if("2".equals(assessment.getGender()) && !"4".equals(assessment.getFemaleMedicalHistory())){
+            if(assessment.getFemaleMedicalHistory().contains("1")) list.add( "有巨大儿（出生体重>=4KG）生产史");
+            if(assessment.getFemaleMedicalHistory().contains("2")) list.add( "有妊娠期糖尿病史");
+            if(assessment.getFemaleMedicalHistory().contains("3")) list.add( "多囊卵巢综合症患者");
         }
         return list;
     }
