c64f24284d638a06223d015d4107517ea026f1c0
zhongshuqing <zhongshuqing@wondersgroup.com>
2017-07-13 17:33:52 +0800

    bugFix

--- neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/system/OverAuthExcludesRepository.java
+++ neohealthcloud-server-jpa/src/main/java/com/wondersgroup/healthcloud/jpa/repository/system/OverAuthExcludesRepository.java
@@ -3,10 +3,11 @@ package com.wondersgroup.healthcloud.jpa.repository.system;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.wondersgroup.healthcloud.jpa.entity.system.OverAuthExcludes;
 
 public interface OverAuthExcludesRepository extends JpaRepository<OverAuthExcludes, Integer> {
-    
+    @Query(nativeQuery = true,value="SELECT t.excludesPath FROM tb_overauth_excludes t WHERE t.type=?1")
     List<String> queryExcludesPathByType(String type);
 }
